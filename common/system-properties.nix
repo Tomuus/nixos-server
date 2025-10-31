dee2561414bdbd291847084a5686c73f1cf50678
Tomus <tomasz1.mrozinski@proton.me>
2025-10-31 19:15:07 +0100

    Changed bootloader
    Yeah the vm is gonna be efi

--- common/system-properties.nix
+++ common/system-properties.nix
@@ -3,7 +3,7 @@
 {
   nix.settings.experimental-features = ["nix-command" "flakes"];
 
-  boot.loader.grub.enable = false;
+  boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.networkmanager.enable = true;
