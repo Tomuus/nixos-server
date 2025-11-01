123d500df18993a89af96b02cbf0b7ba157e2776
Tomus <tomasz1.mrozinski@proton.me>
2025-11-01 09:19:18 +0100

    Changing forgejo settings part 1/1e9
    Please work

--- hosts/forgejo/server.nix
+++ hosts/forgejo/server.nix
@@ -6,6 +6,13 @@
     settings.server.DOMAIN = "forge.tomuus.org";
     settings.server.ROOT_URL = "https://forge.tomuus.org/";
     settings.git.SSH_DOMAIN = "forge.tomuus.org";
+    settings = {
+      service = {
+        DISABLE_REGISTRATION = true;
+        ENABLE_CAPTCHA = true;
+        APP_NAME = "Coding goes brrrrrr";
+      }
+    }
   };
 
   networking.firewall.allowedTCPPorts = [ 22 3000 ];
