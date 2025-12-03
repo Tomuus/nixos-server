0b79e21c0b211251fa728f56cc5b7d8a9b3bf153
Tomus <tomasz1.mrozinski@proton.me>
2025-12-03 15:50:46 +0100

    Made zfs mount at boot

--- modules/smb/drives.nix
+++ modules/smb/drives.nix
@@ -3,4 +3,16 @@
 {
   boot.supportedFilesystems = [ "zfs" ];
   networking.hostId = "a1f9c2e3";
+
+  boot.zfs.extraPools = [ "jedentb" "piectb" ];
+  
+  fileSystems."/shares/jedentb" = {
+    device = "jedentb";
+    fsType = "zfs";
+  };
+
+  fileSystems."/shares/piectb" = {
+    device = "piectb";
+    fsType = "zfs";
+  };
 }
