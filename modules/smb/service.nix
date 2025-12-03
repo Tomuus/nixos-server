b432951e3638368254eaf5f65eac3918e53e13b6
Tomus <tomasz1.mrozinski@proton.me>
2025-12-03 19:29:43 +0100

    Truenas tuning

--- modules/smb/service.nix
+++ modules/smb/service.nix
@@ -13,6 +13,21 @@
         #yeah idk how the 2 lines under this work but these were in the tutorial
         "guest account" = "nobody";
         "map to guest" = "bad user";
+
+        "aio read size" = "1";
+        "aio write size" = "1";
+
+        "strict sync" = "no";
+        "sync always" = "no";
+
+        "min receivefile size" = "16384";
+        "read raw" = "yes";
+        "write raw" = "yes";
+
+        "use sendfile" = "yes";
+
+        "server min protocol" = "SMB2";
+        "server multi channel support" = "yes";
       };
     };
   };
