0703893f2fc0cc1360bb156762bc116bee22197d
Tomasz Mrozinski <tomasz1.mrozinski@proton.me>
2026-04-16 22:40:37 +0200

    Added nextcloud

---
+++ modules/nextcloud.nix
@@ -0,0 +1,32 @@
+{ config, pkgs, ... }:
+
+{
+  # Enable Nextcloud
+  services.nextcloud = {
+    enable = true;
+    hostName = "100.97.47.41";
+    
+		datadir = "/shares/megaraid";
+
+    config = {
+      adminpassFile = "/run/secrets/nextcloud-admin-pass";  
+      adminuser = "admin";
+      dbtype = "pgsql";  
+    };
+    
+    database.createLocally = true; 
+  };
+
+  # Enable PostgreSQL
+  services.postgresql.enable = true;
+
+	systemd.services.nextcloud-setup = {
+		after = [ "mnt-shares-piectb.mount" "mnt-shares-megaraid.mount" ];
+		requires = [ "mnt-shares-piectb.mount" "mnt-shares-megaraid.mount" ];
+	};
+
+	systemd.services.nextcloud-setup.serviceConfig.User = "smb";
+	systemd.services.nextcloud-setup.serviceConfig.Group = "samba";
+	systemd.services.phpfpm-nextcloud.serviceConfig.User = "smb";
+	systemd.services.phpfpm-nextcloud.serviceConfig.Group = "samba";
+}
