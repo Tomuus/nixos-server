ec0774ec759fc83bfef1743fbc2d3fc64bc22403
Tomasz Mrozinski <tomasz1.mrozinski@proton.me>
2026-04-17 15:39:41 +0200

    Simplify ownership for nextcloud

--- modules/nextcloud.nix
+++ modules/nextcloud.nix
@@ -1,4 +1,4 @@
-{ config, pkgs, ... }:
+{ lib, pkgs, ... }:
 
 {
   # Enable Nextcloud
@@ -29,6 +29,14 @@
 	};
 
 	users.users.nextcloud.extraGroups = [ "samba" ];
+
+	systemd.services.phpfpm-nextcloud.serviceConfig.User = lib.mkForce "smb";
+	systemd.services.phpfpm-nextcloud.serviceConfig.Group = lib.mkForce "samba";
+	systemd.services.nextcloud-setup.serviceConfig.User = lib.mkForce "smb";
+	systemd.services.nextcloud-setup.serviceConfig.Group = lib.mkForce "samba";
+	systemd.services.nextcloud-cron.serviceConfig.User = lib.mkForce "smb";
+	systemd.services.nextcloud-cron.serviceConfig.Group = lib.mkForce "samba";
+
 
  /*	systemd.services.nextcloud-setup.serviceConfig.User = "smb";
 	systemd.services.nextcloud-setup.serviceConfig.Group = "samba";
