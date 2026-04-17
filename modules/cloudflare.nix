8db80876eb4473ffee38019286f0f481edfc1292
Tomasz Mrozinski <tomasz1.mrozinski@proton.me>
2026-04-17 11:42:11 +0200

    Cloudflared

--- modules/cloudflare.nix
+++ modules/cloudflare.nix
@@ -3,6 +3,16 @@
 {
 	services.cloudflared = {
 		enable = true;
+
+		tunnels = {
+			"5ac4b348-f410-49cc-a3d5-2a6b22020867" = {
+				credentialsFile = "/etc/cloudflared/5ac4b348-f410-49cc-a3d5-2a6b22020867.json";
+				default = "http_status:404";
+				ingress = {
+					"nextcloud.tomuus.org" = "http://localhost:80";
+				};
+			};
+		};
 	};
 
 	environment.systemPackages = with pkgs; [
