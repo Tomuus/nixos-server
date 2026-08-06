b6fc11af798d1f73092700cfe0535d6635f8a3d5
Mikołaj Iwulski <miwulski@icloud.com>
2026-08-06 13:57:33 +0200

    Allow macOS NFS clients and expand Tailscale IP range

--- modules/nfs.nix
+++ modules/nfs.nix
@@ -3,16 +3,16 @@
 {
   services.nfs.server.enable = true;
   services.nfs.server.exports = ''
-    /shares/bigblack 100.96.47.47(rw,sync,no_subtree_check,all_squash,anonuid=1002,anongid=991,fsid=1)
-    /shares/megaraid 100.96.47.47(rw,sync,no_subtree_check,crossmnt,all_squash,anonuid=1002,anongid=991,fsid=2)
-    /shares/piectb 100.96.47.47(rw,sync,no_subtree_check,crossmnt,all_squash,anonuid=1002,anongid=991,fsid=3)
+    /shares/bigblack 100.0.0.0/8(rw,sync,no_subtree_check,all_squash,anonuid=1002,anongid=991,insecure,fsid=1)
+    /shares/megaraid 100.0.0.0/8(rw,sync,no_subtree_check,crossmnt,all_squash,anonuid=1002,anongid=991,insecure,fsid=2)
+    /shares/piectb 100.0.0.0/8(rw,sync,no_subtree_check,crossmnt,all_squash,anonuid=1002,anongid=991,insecure,fsid=3)
 
-    /shares/bigblack 192.168.0.0/24(rw,sync,no_subtree_check,all_squash,anonuid=1002,anongid=991,fsid=1)
-    /shares/megaraid 192.168.0.0/24(rw,sync,no_subtree_check,crossmnt,all_squash,anonuid=1002,anongid=991,fsid=2)
-    /shares/piectb 192.168.0.0/24(rw,sync,no_subtree_check,crossmnt,all_squash,anonuid=1002,anongid=991,fsid=3)
+    /shares/bigblack 192.168.0.0/24(rw,sync,no_subtree_check,all_squash,anonuid=1002,anongid=991,insecure,fsid=1)
+    /shares/megaraid 192.168.0.0/24(rw,sync,no_subtree_check,crossmnt,all_squash,anonuid=1002,anongid=991,insecure,fsid=2)
+    /shares/piectb 192.168.0.0/24(rw,sync,no_subtree_check,crossmnt,all_squash,anonuid=1002,anongid=991,insecure,fsid=3)
   '';
 
-  #Copied straight out of nixos wiki
+  # Copied straight out of nixos wiki
 
   services.nfs.server = {
     # fixed rpc.statd port; for firewall
