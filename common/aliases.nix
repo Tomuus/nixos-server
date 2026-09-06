b504c1006b75c93b2cde34583285e2c35b9e76e3
serweradminpro69 <mikiiwu@Macbook-Air-Mikolaj.local>
2026-09-06 10:24:37 +0200

    nwm jak robic amend wiec robie normalnie sry tomus

--- common/aliases.nix
+++ common/aliases.nix
@@ -17,7 +17,7 @@
         sleep 1
 
       tmux new-session -d -s ortho4xp-gui \
-      'Xvfb :1 -screen 0 1920x1080x24 & sleep 1 && DISPLAY=:1 x11vnc -display :1 -forever -shared -localhost -passwd ortho4xp & sleep 1 && cd /var/lib/ortho4xp-bundled && DISPLAY=:1 steam-run ./Ortho4XP'x
+      'Xvfb :1 -screen 0 1920x1080x24 & sleep 1 && DISPLAY=:1 x11vnc -display :1 -forever -shared -localhost -passwd ortho4xp & sleep 1 && cd /var/lib/ortho4xp-bundled && DISPLAY=:1 steam-run ./Ortho4XP'
         echo "ortho4xp-gui session started. Attach with: tmux attach -t ortho4xp-gui"
     }
   '';
